bd38f14a3b7b90b4e1a41446a12588efc0a6cd53
H0MZ0 <hamzared2500@gmail.com>
2025-02-05 22:45:22 +0100

    p

---
+++ Makefile
@@ -0,0 +1,28 @@
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+
+CLN = client.c
+SRV = server.c
+
+COBJ = $(CLN:.c=.o)
+SOBJ = $(SRV:.c=.o)
+
+# NAME1 = client
+NAME2 = server
+
+all : $(NAME2)
+
+# $(NAME1): $(COBJ) $(NAME2)
+# 	$(CC) $(COBJ) -o $(NAME1)
+
+$(NAME2): $(SOBJ)
+	$(CC) $(SOBJ) -o $(NAME2)
+
+clean : 
+	rm $(COBJ) $(SOBJ)
+fclean : clean
+	rm $(NAME1) $(NAME2)
+
+re: fclean all
+
+# .PHONY all clean fclean re
---
+++ minitalk.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hakader <hakader@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/05 15:28:53 by hakader           #+#    #+#             */
+/*   Updated: 2025/02/05 15:54:37 by hakader          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "GNL/get_next_line.h"
+# include <signal.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <stdlib.h>
+
+
+// size_t	ft_strlen(char *str);
+
+
+#endif
