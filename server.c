c7149a75f7697bdfc54466d0a6dda8c96eebd063
H0MZ0 <hamzared2500@gmail.com>
2025-02-07 19:31:17 +0100

    p

--- server.c
+++ server.c
@@ -6,11 +6,22 @@
 /*   By: hakader <hakader@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:28:49 by hakader           #+#    #+#             */
-/*   Updated: 2025/02/07 17:14:44 by hakader          ###   ########.fr       */
+/*   Updated: 2025/02/07 19:31:13 by hakader          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+int	po2(int *num)
+{
+	int	i;
+
+	i = 0;
+	while (i <= 7)
+	{
+
+	}
+}
 
 int main()
 {
@@ -19,6 +30,8 @@ int main()
 	get = getpid();
 
 	printf("PID : -> {%d}\n", get);
+	signal();
+	
 	while (1)
 	{
 		sleep(2);
